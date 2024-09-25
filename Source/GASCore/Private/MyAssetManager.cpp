ce2b52bebb12b2b591cbad5a64479fb47ead3f68
Dylan-JM <dylanjmarley01@gmail.com>
2024-09-25 21:07:21 +0100

    Asset manager moved

--- Source/GASCore/Private/MyAssetManager.cpp
+++ Source/GASCore/Private/MyAssetManager.cpp
@@ -1,7 +1,7 @@
 // Fill out your copyright notice in the Description page of Project Settings.
 
 
-#include "AbilitySystem/MyAssetManager.h"
+#include "MyAssetManager.h"
 #include "AbilitySystemGlobals.h"
 
 UMyAssetManager& UMyAssetManager::Get()
